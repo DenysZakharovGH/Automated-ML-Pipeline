eefa5666c0ffb87ddddf32a0dbf193e1dc135871
DenysZakharovGH <deniszaharov19981208@gmail.com>
2025-11-22 21:54:17 +0100

    project structure

---
+++ main.py
@@ -0,0 +1,27 @@
+# what is Pipeline really is
+# what is a difference with templates
+#
+
+# Special features
+# 1. MLflow
+
+
+
+# 1. Preprocessing
+    # 1.1 deal with None
+    # 1.2 Normalisation
+    # 1.2 Category codling ??
+# 2. Feature engineering
+    # 2.1 Add new feature
+    # 2.1 Delete old feature
+# 3. Model Selection
+    # 3.1 Choose between paar model to taste
+# 4. Hyperparameter tuning
+    # 4.1 GridSearchCV
+    # 4.2 RandomizedSearchCV
+# 5. Evaluation
+    # 5.1 Metrics
+    # 5.2 graf
+# 6. Save + load pipeline
+    # 6.1 joblib
+    # 6.2 pickle
