15ba1faa43755d68ff29db753d846d5a82af39aa
DenysZakharovGH <deniszaharov19981208@gmail.com>
2025-11-23 21:25:02 +0100

    add a path keeper file -> later will be changed with settings file

--- utils/corrilations.py
+++ utils/corrilations.py
@@ -1,4 +1,6 @@
 import sns
 
+from utils.path_keeper import csvFile
+
 # seaborn to check any corrilations between variables
 scatter_fig = sns.pairplot(csvFile, kind="scatter", plot_kws={"alpha":0.4})
