91bd2d32b7beff45df2eb91f904704d958e54256
DenysZakharovGH <deniszaharov19981208@gmail.com>
2025-11-26 22:35:02 +0100

    add a path keeper file -> later will be changed with settings file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # what is a difference with templates
 #
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
+from pipeline.preprocess import create_pipeline
 from utils.path_keeper import path_to_csv
 
 # Special features
@@ -13,7 +15,7 @@ from utils.path_keeper import path_to_csv
 # 1. Preprocessing
     # 1.1 deal with None
     # 1.2 Normalisation
-    # 1.2 Category codling (convert strings to numbers)
+    # 1.3 Category codling (convert strings to numbers)
 # 2. Feature engineering
     # 2.1 Add new feature
     # 2.1 Delete old feature
@@ -46,9 +48,28 @@ from utils.path_keeper import path_to_csv
 # 1. first tast to test it with Model I wrote before alone
 # titanic case
 
-
+# 1. Preprocessing
 csv_data = pd.read_csv(path_to_csv)
 
+# 1.1 deal with None
+csv_data = csv_data.dropna()
 
+columns_lst = csv_data.columns.to_list()
 
-print(csv_data.info())
+features = columns_lst[:-1]
+target = columns_lst[-1]
+
+# Define target and features
+X = csv_data[features]
+y = csv_data[target]
+
+# Split into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(f"Training set shape: {X_train.shape}")
+print(f"Testing set shape: {X_test.shape}")
+
+pipeline = create_pipeline()
+
+pipeline.fit(X_train, y_train)
+print("Model training complete!")
--- pipeline/preprocess.py
+++ pipeline/preprocess.py
@@ -3,6 +3,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier
 
+
+
+# 1.2 Normalisation
+# 1.3 Category codling (convert strings to numbers)
 def create_pipeline():
     pipeline = Pipeline([
         ("imputer", SimpleImputer(strategy="mean")),   # заповнюємо пропуски
